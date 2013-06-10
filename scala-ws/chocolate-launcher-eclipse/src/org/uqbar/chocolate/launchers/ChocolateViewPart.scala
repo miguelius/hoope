5e76943a0fd44d9a3248484656ceeb5a080bc7d8
Miguel Carboni <miguel.carboni@gmail.com>
2013-06-10 01:39:51 -0300

    agregado el frankestein que muestra games chocolate dentro de eclipse/scala

---
+++ scala-ws/chocolate-launcher-eclipse/src/org/uqbar/chocolate/launchers/ChocolateViewPart.scala
@@ -0,0 +1,46 @@
+package org.uqbar.chocolate.launchers
+
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.ui.part.ViewPart
+import javax.swing.JFrame
+import org.uqbar.chocolate.core.GamePlayer
+import java.awt.event.WindowListener
+import java.awt.event.WindowEvent
+import org.uqbar.chocolate.core.Game
+import org.uqbar.chocolate.games.test._
+import org.eclipse.swt.SWT
+import org.eclipse.swt.awt.SWT_AWT
+
+class ChocolateViewPart extends ViewPart {
+
+   // acá defino el juego
+   val player = new GamePlayer(KeyboardTest)
+  
+   override def createPartControl(parent : Composite) {
+		var swtAwtComponent = new Composite(parent, SWT.EMBEDDED);
+		    
+		var frame = SWT_AWT.new_Frame( swtAwtComponent )
+		frame.add(player)
+		
+		frame.setLocationRelativeTo(null)
+		frame.setVisible(true)
+		frame.setResizable(false)
+		frame.pack
+		frame.setTitle(player.game.title)
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+		
+		frame.addWindowListener(new WindowListener {
+				def windowActivated(e : WindowEvent) = player.resume
+				def windowDeactivated(e : WindowEvent) = player.pause
+				def windowOpened(e : WindowEvent) {}
+				def windowIconified(e : WindowEvent) {}
+				def windowDeiconified(e : WindowEvent) {}
+				def windowClosing(e : WindowEvent) {}
+				def windowClosed(e : WindowEvent) {}
+			})
+   }
+  
+   override def setFocus() {
+	   player.resume
+   }
+}
---
+++ scala-ws/chocolate-launcher-eclipse/target/classes/org/uqbar/chocolate/launchers/MockGame.scala
@@ -0,0 +1,12 @@
+package org.uqbar.chocolate.launchers
+
+import org.uqbar.chocolate.core.reactions.ReactionRegistry
+import org.uqbar.chocolate.games.test.components.Ball
+import org.uqbar.chocolate.core.reactions.events.UpdateEvent
+
+object MockGame extends org.uqbar.chocolate.core.Game {
+
+    def title = "Oh no"
+	def displaySize = (600, 600)
+	
+}
