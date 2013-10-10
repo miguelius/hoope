2a07eba595d557b6a873f2abd7da300198bb65b0
Miguel Carboni <miguel.carboni@gmail.com>
2013-10-09 23:24:31 -0300

    agrego el enganche entre interprete y viewpart

---
+++ sancristobal/org.uqbar.hoope.lib/src/org/uqbar/hoope/lib/IHoopePlayground.java
@@ -0,0 +1,7 @@
+package org.uqbar.hoope.lib;
+
+import org.eclipse.emf.ecore.EObject;
+
+public interface IHoopePlayground {
+	void registerGraphicObject(EObject objectMetadata, Object realObject);
+}
